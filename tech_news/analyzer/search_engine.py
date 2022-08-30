a56f42675ec94365e3f4097678edfa8390ba1afd
vinicius <vinicius.savordelli@gmail.com>
2022-08-30 17:00:57 -0300

    feat(search_engine): implements search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 6
@@ -9,7 +10,12 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        formated_date = datetime.fromisoformat(date).strftime("%d/%m/%Y")
+        filtered_news = search_news({"timestamp": formated_date})
+        return [(i["title"], i["url"]) for i in filtered_news]
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 8
