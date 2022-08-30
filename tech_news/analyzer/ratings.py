716be46aafd92a378e8552929d18dfeb54dccf18
vinicius <vinicius.savordelli@gmail.com>
2022-08-30 17:17:30 -0300

    feat(ratings): implements top_5_news

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,15 @@
+from tech_news.database import get_collection
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    filtered_news = list(
+        get_collection()
+        .find({}, {"_id": False})
+        .sort([("comments_count", -1), ("title", 1)])
+        .limit(5)
+    )
+    return [(news["title"], news["url"]) for news in filtered_news]
 
 
 # Requisito 11
