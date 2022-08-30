d6122340c61cc9029d5a607755e00408f3f3d9c2
vinicius <vinicius.savordelli@gmail.com>
2022-08-30 17:05:00 -0300

    refactor(search_engine): replaces i for a more descritive variable name in the for loops

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -5,7 +5,7 @@ from datetime import datetime
 # Requisito 6
 def search_by_title(title):
     filtered_news = search_news({"title": {"$regex": title, "$options": "i"}})
-    return [(i["title"], i["url"]) for i in filtered_news]
+    return [(news["title"], news["url"]) for news in filtered_news]
 
 
 # Requisito 7
@@ -13,7 +13,7 @@ def search_by_date(date):
     try:
         formated_date = datetime.fromisoformat(date).strftime("%d/%m/%Y")
         filtered_news = search_news({"timestamp": formated_date})
-        return [(i["title"], i["url"]) for i in filtered_news]
+        return [(news["title"], news["url"]) for news in filtered_news]
     except ValueError:
         raise ValueError("Data inválida")
 
@@ -21,7 +21,7 @@ def search_by_date(date):
 # Requisito 8
 def search_by_tag(tag):
     filtered_news = search_news({"tags": {"$regex": tag, "$options": "i"}})
-    return [(i["title"], i["url"]) for i in filtered_news]
+    return [(news["title"], news["url"]) for news in filtered_news]
 
 
 # Requisito 9
