1f9a5fcbf1ad78e85bc36e8dc05ed747b944a2a2
vinicius <vinicius.savordelli@gmail.com>
2022-08-30 16:57:26 -0300

    feat(search_engine): implements search_by_title

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,10 @@
+from tech_news.database import search_news
+
+
 # Requisito 6
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    filtered_news = search_news({"title": {"$regex": title, "$options": "i"}})
+    return [(i["title"], i["url"]) for i in filtered_news]
 
 
 # Requisito 7
